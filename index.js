a5d6f58ba9032a6849412d4d1b5a47058e4b8aa0
Noxlobin <aadrijamaitydps@gmail.com>
2024-02-08 15:08:25 +0530

    added year

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const date = new Date();
 export default {
   install: (app, options) => {
     console.log(`time:${date.getMilliseconds()}`);
+
+    app.provide("year", date.getFullYear());
   },
 };
