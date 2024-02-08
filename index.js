e8be405cbbfc7d2370259482975a463f4fa6d51d
Noxlobin <aadrijamaitydps@gmail.com>
2024-02-08 15:54:04 +0530

    added rest of the stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const date = new Date();
 
 export default {
   install: (app, options) => {
-    console.log(`time:${date.getMilliseconds()}`);
+    console.log(`time: ${Date.now()}`);
 
     app.directive("current-year", {
       beforeMount: (el, binding) => {
@@ -10,6 +10,38 @@ export default {
       },
       updated: (el, binding) => {
         el.innerText = date.getFullYear();
+      },
+    });
+    app.directive("current-month", {
+      beforeMount: (el, binding) => {
+        el.innerText = date.getMonth();
+      },
+      updated: (el, binding) => {
+        el.innerText = date.getMonth();
+      },
+    });
+    app.directive("current-day", {
+      beforeMount: (el, binding) => {
+        el.innerText = date.getDate();
+      },
+      updated: (el, binding) => {
+        el.innerText = date.getDate();
+      },
+    });
+    app.directive("current-hour", {
+      beforeMount: (el, binding) => {
+        el.innerText = date.getHours();
+      },
+      updated: (el, binding) => {
+        el.innerText = date.getHours();
+      },
+    });
+    app.directive("current-minute", {
+      beforeMount: (el, binding) => {
+        el.innerText = date.getMinutes();
+      },
+      updated: (el, binding) => {
+        el.innerText = date.getMinutes();
       },
     });
   },
